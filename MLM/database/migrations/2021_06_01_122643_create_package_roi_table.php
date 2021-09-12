cdb7128e3c5e017380a1a5c1562cc3fbece70426
hamid <hamire3a@gmail.com>
2021-09-12 20:13:58 +0430

    fix migration issue

--- MLM/database/migrations/2021_06_01_122643_create_package_roi_table.php
+++ MLM/database/migrations/2021_06_01_122643_create_package_roi_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ُCreatePackageRoiTable extends Migration
+class CreatePackageRoiTable extends Migration
 {
     /**
      * Run the migrations.
