81881dec3c176170ffc32a3f13def3eaf69d0bc5
NimaN2D <nima.nouri.d@gmail.com>
2021-09-25 14:05:28 +0330

    Update GRPC

--- MLM/Services/Grpc/MLMServiceInterface.php
+++ MLM/Services/Grpc/MLMServiceInterface.php
@@ -6,8 +6,8 @@ namespace MLM\Services\Grpc;
 
 use Mix\Grpc;
 use Mix\Grpc\Context;
-use Orders\Services\Grpc;
-use User\Services\Grpc;
+use Orders\Services\Grpc as OrderGrpc;
+use User\Services\Grpc as UserGrpc;
 
 interface MLMServiceInterface extends Grpc\ServiceInterface
 {
@@ -16,37 +16,37 @@ interface MLMServiceInterface extends Grpc\ServiceInterface
 
     /**
     * @param Context $context
-    * @param Grpc\User $request
+    * @param UserGrpc $request
     * @return Acknowledge
     *
     * @throws Grpc\Exception\InvokeException
     */
-    public function hasValidPackage(Context $context, Grpc\User $request): Acknowledge;
+    public function hasValidPackage(Context $context, UserGrpc $request): Acknowledge;
 
     /**
     * @param Context $context
-    * @param Grpc\User $request
+    * @param OrderGrpc $request
     * @return Rank
     *
     * @throws Grpc\Exception\InvokeException
     */
-    public function getUserRank(Context $context, Grpc\User $request): Rank;
+    public function getUserRank(Context $context, UserGrpc $request): Rank;
 
     /**
     * @param Context $context
-    * @param Grpc\Order $request
+    * @param OrderGrpc $request
     * @return Acknowledge
     *
     * @throws Grpc\Exception\InvokeException
     */
-    public function simulateOrder(Context $context, Grpc\Order $request): Acknowledge;
+    public function simulateOrder(Context $context, UserGrpc $request): Acknowledge;
 
     /**
     * @param Context $context
-    * @param Grpc\Order $request
+    * @param OrderGrpc $request
     * @return Acknowledge
     *
     * @throws Grpc\Exception\InvokeException
     */
-    public function submitOrder(Context $context, Grpc\Order $request): Acknowledge;
+    public function submitOrder(Context $context, OrderGrpc $request): Acknowledge;
 }
