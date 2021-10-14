ba1e2ebfe50b1f0fdef1b0d444c8b2748fd1c3bd
hamid <hamire3a@gmail.com>
2021-10-14 18:33:35 +0330

    sponsor

--- MLM/Http/Controllers/Front/MLMController.php
+++ MLM/Http/Controllers/Front/MLMController.php
@@ -6,6 +6,7 @@ namespace MLM\Http\Controllers\Front;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use MLM\Http\Requests\BinaryTreeMultiRequest;
+use MLM\Http\Requests\MLMInfoRequest;
 use MLM\Http\Requests\ReferralTreeMultiRequest;
 use MLM\Models\ReferralTree;
 use MLM\Models\Tree;
@@ -20,18 +21,25 @@ class MLMController extends Controller
      * @group
      * Public User > User Info
      *
+     * @queryParam id integer
      */
-    public function getMLMInfo()
+    public function getMLMInfo(MLMInfoRequest $request)
     {
         /** @var  $user User */
-        $user = auth()->user();
-        if (! $user->hasBinaryNode()) {
+        if ($request->has('id') && request('id'))
+            $user = User::query()->find(request('id'));
+        else
+            $user = auth()->user();
+
+
+        if (!$user->hasBinaryNode()) {
             $info = [
                 'level' => 0,
                 'converted_points' => 0,
                 'left_leg_points' => 0,
                 'right_leg_points' => 0,
 
+                'sponsor_user' => $user->sponsor,
                 'highest_package_detail' => $user->biggestActivePackage(),
                 'highest_package' => optional($user->biggestActivePackage())->package,
                 'rank' => $user->rank_model
@@ -47,6 +55,8 @@ class MLMController extends Controller
                 'right_leg_points' => $binary_tree->rightSideChildrenPackagePrice(),
                 'children_count_left' => $binary_tree->leftChildCount(),
 
+
+                'sponsor_user' => $user->sponsor,
                 'highest_package_detail' => $user->biggestActivePackage(),
                 'highest_package' => optional($user->biggestActivePackage())->package,
                 'rank' => $user->rank_model
