feb5b689132b0c9bf1a0da2054ff77bd86f154be
NimaN2D <nima.nouri.d@gmail.com>
2021-11-28 12:54:07 +0330

    Add country & country_iso2 to User model

---
+++ User/database/migrations/2021_11_28_123559_add_country_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCountryToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('country')->after('block_type')->nullable();
+            $table->string('country_iso2')->after('country')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
